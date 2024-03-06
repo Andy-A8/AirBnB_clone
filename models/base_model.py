b78d397ed3092e33c1e7ce45fa3bc9b255a2573e
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-06 08:12:32 -0800

    Updated base model file and passed base model test

--- models/base_model.py
+++ models/base_model.py
@@ -6,35 +6,31 @@ from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of this project"""
+    """Represents the base model of this project"""
 
+    id = str(uuid.uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
-id = str(uuid.uuid4())
-created_at = datetime.now
-updated_at = datetime.now
+    def __str__(self):
+        """Returns string representation of BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
+    def save(self):
+        """Updates the pi attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
 
-def __str__(self):
-    """Returns string representation of BaseModel instance"""
-    return "[{}] ({}) {}".format(self.__class__.__name__,
-                                 self.id, self.__dict__)
+    def to_dict(self):
+        """Returns a dictionary with all keys/values of__dict__of the instance:
+            instance attributes: created_at and cupdated_at
+            a key __class__ must be added with the class name of the object
+            inst.attributes must be converted to string object in IOS format
+        """
 
+        the_dict = self.__dict__.copy()
+        the_dict["__class__"] = self.__class__.__name__
+        the_dict["created_at"] = self.created_at.isoformat()
+        the_dict["updated_at"] = self.updated_at.isoformat()
 
-def save(self):
-    """Updates the pi attribute updated_at with the current datetime"""
-    self.updated_at = datetime.now()
-
-
-def to_dict(self):
-    """Returns a dictionary with all keys/values of__dict__of the instance:
-        instance attributes: created_at and cupdated_at
-        a key __class__ must be added with the class name of the object
-        instance attributes must be converted to string object in IOS format.
-    """
-
-    the_dict = self.__dict__.copy()
-    the_dict["__class__"] = self.__class__.__name__
-    the_dict["created_at"] = self.created_at.isoformat()
-    the_dict["updated_at"] = self.updated_at.isoformat()
-
-    return the_dict
+        return the_dict
