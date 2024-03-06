58d2331561666ac7ae163fd19d86f98ba00e28ee
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-06 05:04:13 -0800

    Added public instance attribute: id using uuid.uuid4()

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 """Defines a class BaseModel"""
 
+import uuid
+
 
 class BaseModel:
-    """ Represents the base model of this project"""
+    """Represents the base model of this project"""
 
     pass
+
+
+def id(self):
+    """Generate unique id using uuid4()"""
+    self.id = str(uuid.uuid4())
+    return (self.id)
