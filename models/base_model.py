5ea069511236883fbdb536eaebaa60dac135d432
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-06 07:47:09 -0800

    Updated tests

--- models/base_model.py
+++ models/base_model.py
@@ -8,33 +8,21 @@ from datetime import datetime
 class BaseModel:
     """Represents the base model of this project"""
 
-    pass
 
-
-def __init__(self, id, created_at, updated_at):
-    """Generate unique id using uuid4()
-
-    Args:
-        id = unique id.
-        created_at = current datetime of created instance.
-        Updated_at = Current datetime every time an instance is updated.
-    """
-
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.today
-    self.updated_at = datetime.today
+id = str(uuid.uuid4())
+created_at = datetime.now
+updated_at = datetime.now
 
 
 def __str__(self):
     """Returns string representation of BaseModel instance"""
-    return "[{}] ({}) {}".format(self.__class__.__name__
+    return "[{}] ({}) {}".format(self.__class__.__name__,
                                  self.id, self.__dict__)
 
 
 def save(self):
     """Updates the pi attribute updated_at with the current datetime"""
     self.updated_at = datetime.now()
-    return (self.updated_at)
 
 
 def to_dict(self):
