661eb53a2d2b5ffe44fd53316057a41f195da07f
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-06 14:02:46 -0800

    Re-created an instance (BaseModel) with this dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -16,21 +16,21 @@ class BaseModel:
             **kwargs (dict): key/value pairs of attributes.
         """
 
-        if kwargs and len(kwargs) is not o:
+        dt.fmt = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs and len(kwargs) is not 0:
             for key in kwargs:
                 if key == "created_at":
                     self.__dict__["created_at"] = datetime.strptime(
-                            kwargs["created_at"], %Y-%m-%dT%H:%M:%S.%f)
+                            kwargs["created_at"], dt.fmt)
                 elif key == "updated_at":
                     self.__dict__["updated_at"] = datetime.strptime(
-                            kwargs["updated_at"], %Y-%m-%dT%H:%M:%S.%f)
+                            kwargs["updated_at"], dt.fmt)
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
 
     def __str__(self):
         """Returns string representation of BaseModel instance"""
