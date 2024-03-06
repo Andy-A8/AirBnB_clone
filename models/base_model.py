5402597f45c54eb8c75f4c9f209cd1fb1ffa0c95
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-06 10:40:58 -0800

    Re-created a instance (BaseModel) with this dictionary representation: partly done

--- models/base_model.py
+++ models/base_model.py
@@ -8,9 +8,29 @@ from datetime import datetime
 class BaseModel:
     """Represents the base model of this project"""
 
-    id = str(uuid.uuid4())
-    created_at = datetime.now()
-    updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel
+
+        Args:
+            *args: list of argumentsa (unused).
+            **kwargs (dict): key/value pairs of attributes.
+        """
+
+        if kwargs and len(kwargs) is not o:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                            kwargs["created_at"], %Y-%m-%dT%H:%M:%S.%f)
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                            kwargs["updated_at"], %Y-%m-%dT%H:%M:%S.%f)
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         """Returns string representation of BaseModel instance"""
