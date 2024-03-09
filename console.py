6c25a93f2bc602436c640561629778a561ee7c28
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-09 14:46:27 -0800

    Included quit command to exit the program

--- console.py
+++ console.py
@@ -8,6 +8,10 @@ import cmd
 class HBNBCommand(cmd.cmd):
     """Represents the class for the  command interpreter"""
 
+    def do_quit(self, arg):
+        """To exit the program: Quit command"""
+        return True
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
