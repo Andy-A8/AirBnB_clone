eea799564f74656226207a9f7b3f2a90e711f5af
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-19 08:09:52 -0700

    Created and updated class FileStorage with private class attributes

---
+++ models/engine/file_storage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a class FileStorage"""
+
+
+class FileStorage:
+    """Represents a storage engine.
+
+    Attributes:
+        __file_path (str): The file name to save objects.
+        __objects (dict): A dictionary of objects.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
