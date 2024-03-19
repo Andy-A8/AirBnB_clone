fb7ea1967f25f124ebbeaf54b1eb1bfd702c798c
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-19 13:38:10 -0700

    Updated class FileStorage with public instance method: reload

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defines a class FileStorage"""
+import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -11,7 +13,7 @@ class FileStorage:
     """
 
     __file_path = "file.json"
-    __objects = {}
+    __objects = {}i
 
     def all(self):
         """Returns the dictionary __objects"""
@@ -29,3 +31,17 @@ class FileStorage:
             for key, value in FileStorage.__objects.item():
                 obj_dict[key] = value to_dict()
             json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects (only if the JSON fileexists
+            Otherwise, do nothing. If the file doesn’t exist,
+            no exception should be raised).
+        """
+        try:
+            FIleStorage.__objects.clear()
+            with open("file.json", "r") as f:
+                obj_dict = json.load(f)
+                for key, val in obj_dict.items():
+                    FileStorage.__objects[key] = eval(val["__class__"])(**val)
+        except (FileNotFoundError):
+            return
