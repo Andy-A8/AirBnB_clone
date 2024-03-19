5e6b5b9754d1721662d6f2d7ed56c82a3b581486
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-19 07:18:19 -0700

    Updated BaseModel test file with arg and kwargs tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -52,6 +52,30 @@ class TestBaseModel_instantiation(unittest.TestCase):
         self.assertIn("'created_at': " + dt_repr, bm.__str__())
         self.assertIn("'updated_at': " + dt_repr, bm.__str__())
 
+    def test_args_unused(self):
+        bm = BaseModel(None)
+        self.assertNotIn(None, bm.__dict__.values())
+
+    def test_instantiation_with_kwargs(self):
+        dt = datetime.now()
+        dt_iso = dt.isoformat()
+        bm = BaseModel(id="123", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "123")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
+    def test_isntantiation_with_None_kwargs(self):
+        with self.assertRaises(TypeError):
+            BaseModel(id=None, created_at=None, updated_at=None)
+
+    def test_instantiation_with_args_and_kwargs(self):
+        dt = datetime.now()
+        dt_iso = dt.isoformat()
+        bm = BaseModel("12", id="123", created_at=dt_iso, updated_at=dt_iso)
+        self.assertEqual(bm.id, "123")
+        self.assertEqual(bm.created_at, dt)
+        self.assertEqual(bm.updated_at, dt)
+
 
 if __name__ == "__main__":
     unittest.main()
