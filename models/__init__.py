bd7b43de1366e900199be6cc1743fee171080425
Andy-A8 <aaallotey006@st.ug.edu.gh>
2024-03-19 13:54:00 -0700

    Update models magic file

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+"""Create a unique FileStorage instance for your application:
+        Import file_storage.py
+        Create the variable storage, an instance of FileStorage
+        Call reload() method on this variable
+"""
 
+from models.engine.file_storage import FileStorage
+
+storage = FileStorage()
+storage.reload()
